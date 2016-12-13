8bc9f553d264bd572661481afd7dfff214a1cf06
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-12-13 21:15:18 +0100

    Added basic functionality and structure.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
-
-echo "TEST";
-
+require_once('../resources/config.php');
+?>
+<div class="container">
+  <div class="menu">
+    <?php require_once(TEMPLATES_PATH . "/menu.php"); ?>
+  </div>
+  <div class="content">
+    <p>test</p>
+  </div>
+</div>
