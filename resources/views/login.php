a5dc42257eb78dbc0e1431acf177573f3d12b218
Jeroen <grootenjeroen@hotmail.com>
2017-01-12 11:29:37 +0100

    Added check if form fiels are filled in and styled the alerts.

--- resources/views/login.php
+++ resources/views/login.php
@@ -15,53 +15,57 @@ $email = $_POST['email'];
 $wachtwoord = $_POST['wachtwoord'];
 
 if(!empty($_POST)){
-  //Pak het wachtwoordid dat bij de ingevoerde email hoort
-  $stmt = DB::conn()->prepare("SELECT wachtwoordid FROM Klant WHERE email=?");
-  $stmt->bind_param("s", $email);
-  $stmt->execute();
+  if($email && $wachtwoord != ''){
+    //Pak het wachtwoordid dat bij de ingevoerde email hoort
+    $stmt = DB::conn()->prepare("SELECT wachtwoordid FROM Klant WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
 
-  $stmt->bind_result($wachtwoordid);
-  $stmt->fetch();
-  $stmt->close();
+    $stmt->bind_result($wachtwoordid);
+    $stmt->fetch();
+    $stmt->close();
 
-  //Pak het id van de klant dat bij de ingevoerde email hoort
-  $stmt = DB::conn()->prepare("SELECT id FROM Klant WHERE email=?");
-  $stmt->bind_param("s", $email);
-  $stmt->execute();
+    //Pak het id van de klant dat bij de ingevoerde email hoort
+    $stmt = DB::conn()->prepare("SELECT id FROM Klant WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
 
-  $stmt->bind_result($klantId);
-  $stmt->fetch();
-  $stmt->close();
+    $stmt->bind_result($klantId);
+    $stmt->fetch();
+    $stmt->close();
 
-  //Pak de naam van de klant dat bij de ingevoerde email hoort
-  $name_stmt = DB::conn()->prepare("SELECT naam FROM Klant WHERE id=?");
-  $name_stmt->bind_param("i", $klantId);
-  $name_stmt->execute();
+    //Pak de naam van de klant dat bij de ingevoerde email hoort
+    $name_stmt = DB::conn()->prepare("SELECT naam FROM Klant WHERE id=?");
+    $name_stmt->bind_param("i", $klantId);
+    $name_stmt->execute();
 
-  $name_stmt->bind_result($naam);
-  $name_stmt->fetch();
-  $name_stmt->close();
+    $name_stmt->bind_result($naam);
+    $name_stmt->fetch();
+    $name_stmt->close();
 
-  //Haal het wachtwoord op dat bij het ID hoort
-  $ww_stmt = DB::conn()->prepare("SELECT wachtwoord FROM Wachtwoord WHERE id=?");
-  $ww_stmt->bind_param("i", $wachtwoordid);
-  $ww_stmt->execute();
+    //Haal het wachtwoord op dat bij het ID hoort
+    $ww_stmt = DB::conn()->prepare("SELECT wachtwoord FROM Wachtwoord WHERE id=?");
+    $ww_stmt->bind_param("i", $wachtwoordid);
+    $ww_stmt->execute();
 
-  $ww_stmt->bind_result($ww);
-  $ww_stmt->fetch();
-  $ww_stmt->close();
+    $ww_stmt->bind_result($ww);
+    $ww_stmt->fetch();
+    $ww_stmt->close();
 
-  //Controlleer het opgehaalde wachtwoord met het ingevoerde wachtwoord overeenkomt
-  if (password_verify($wachtwoord, $ww)) {
-      $_SESSION['login'] = array();
-      $_SESSION['login'][] = $klantId; //Zet de session die we kunnen checken in de functie isIngelogd();
-      $_SESSION['login'][] = $naam;
-      ?>
-      <script>window.location.replace("/");</script>
-      <?php
-  } else {
-      echo 'Wachtwoord klopt NIET!';
+    //Controlleer het opgehaalde wachtwoord met het ingevoerde wachtwoord overeenkomt
+    if (password_verify($wachtwoord, $ww)) {
+        $_SESSION['login'] = array();
+        $_SESSION['login'][] = $klantId; //Zet de session die we kunnen checken in de functie isIngelogd();
+        $_SESSION['login'][] = $naam;
+        ?>
+        <script>window.location.replace("/");</script>
+        <?php
+    } else {
+        echo '<div class="alert">Deze email en wachtwoord combinatie is niet bij ons geregistreerd.</div>';
+    }
+
+    DB::conn()->close();
+  }else{
+    echo '<div class="alert">Controlleer of u alle velden correct heeft ingevuld.</div>';
   }
-
-  DB::conn()->close();
 }
--- resources/views/registreer.php
+++ resources/views/registreer.php
@@ -18,35 +18,40 @@
 <?php
 
 if(!empty($_POST)){
-  //Ingevoerde gegevens aan variabelen assignen
-  $naam = $_POST['naam'];
-  $adres = $_POST['adres'];
-  $postcode = $_POST['postcode'];
-  $woonplaats = $_POST['woonplaats'];
-  $telefoonnummer = $_POST['telefoonnummer'];
-  $email = $_POST['email'];
-  $wachtwoord = $_POST['wachtwoord'];
+  if($_POST['naam'] && $_POST['adres'] && $_POST['postcode']  && $_POST['woonplaats'] && $_POST['telefoonnummer'] && $_POST['email'] && $_POST['wachtwoord'] != ''){
+    //Ingevoerde gegevens aan variabelen assignen
+    $naam = $_POST['naam'];
+    $adres = $_POST['adres'];
+    $postcode = $_POST['postcode'];
+    $woonplaats = $_POST['woonplaats'];
+    $telefoonnummer = $_POST['telefoonnummer'];
+    $email = $_POST['email'];
+    $wachtwoord = $_POST['wachtwoord'];
 
-  //Willekeurig id
-  $id = rand(1, 1100);
+    //Willekeurig id
+    $id = rand(1, 1100);
 
-  // WACHTWOORD
-  //Hash Wachtwoord
-  $hash = password_hash($wachtwoord, PASSWORD_DEFAULT);
-  //Wachtwoord invoeren in tabel Wachtwoord, met een random id
-  $passw_stmt = DB::conn()->prepare("INSERT INTO Wachtwoord (id, wachtwoord) VALUES (?, ?)");
-  $passw_stmt->bind_param("is", $id, $hash);
-  $passw_stmt->execute();
-  echo "Wachtwoord aangemaakt <br>";
+    // WACHTWOORD
+    //Hash Wachtwoord
+    $hash = password_hash($wachtwoord, PASSWORD_DEFAULT);
+    //Wachtwoord invoeren in tabel Wachtwoord, met een random id
+    $passw_stmt = DB::conn()->prepare("INSERT INTO Wachtwoord (id, wachtwoord) VALUES (?, ?)");
+    $passw_stmt->bind_param("is", $id, $hash);
+    $passw_stmt->execute();
 
-  //ACCOUNT GEGEVENS
-  //Gegevens invoeren in Klant tabel
-  $stmt = DB::conn()->prepare("INSERT INTO Klant (naam, adres, postcode, woonplaats, telefoonnummer, email, wachtwoordid) VALUES (?, ?, ?, ?, ?, ?, ?)");
-  $stmt->bind_param("ssssssi", $naam, $adres, $postcode, $woonplaats, $telefoonnummer, $email, $id);
-  $stmt->execute();
-  echo "Account aangemaakt";
-
-  //Connecties afsluiten
-  $stmt->close();
-  DB::conn()->close();
+    //ACCOUNT GEGEVENS
+    //Gegevens invoeren in Klant tabel
+    $stmt = DB::conn()->prepare("INSERT INTO Klant (naam, adres, postcode, woonplaats, telefoonnummer, email, wachtwoordid) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssi", $naam, $adres, $postcode, $woonplaats, $telefoonnummer, $email, $id);
+    $stmt->execute();
+    echo "<div class='succes'>Account aangemaakt.</div>";
+    ?>
+    <script>window.location.replace("/login");</script>
+    <?php
+    //Connecties afsluiten
+    $stmt->close();
+    DB::conn()->close();
+  }else{
+    echo "<div class='alert'>Controlleer of u alle informatie correct heeft ingevuld.</div>";
+  }
 }
