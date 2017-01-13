0f95194598cbf27b45e2444ee31db6cf9a955762
Jeroen <grootenjeroen@hotmail.com>
2017-01-13 11:17:38 +0100

    Pachted the patch

--- resources/layouts/default.php
+++ resources/layouts/default.php
@@ -7,10 +7,11 @@ session_start();
 // }else{
 //   echo "nope";
 // }
-$if(!empty($_SESSION['login'])){
-  $klantId = $_SESSION['login'][0];
-  $klantNaam = $_SESSION['login'][1];
-}
+// $if(!empty($_SESSION)){
+//   $klantId = $_SESSION['login'][0];
+//   $klantNaam = $_SESSION['login'][1];
+//
+// }
 
 function isEigenaar($klantId){
   if($klantId === 1){
