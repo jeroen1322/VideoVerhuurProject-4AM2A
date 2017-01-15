a728cd3c7aa13705b6b6a4208b8cbcf0c5042b58
Jeroen <grootenjeroen@hotmail.com>
2017-01-15 01:40:51 +0100

    Made greate progress on the winkelmand

--- resources/views/winkelmand.php
+++ resources/views/winkelmand.php
@@ -12,42 +12,89 @@ if(!empty($_SESSION['login'])){
   $stmt->execute();
 
   $stmt->bind_result($order_id);
-  $stmt->fetch();
+
+  $orderIdResult = array();
+
+  while($stmt->fetch()){
+    $orderIdResult[] = $order_id;
+  }
+
   $stmt->close();
 
-  //Haal exemplaarid van Orderregel dat bij de Order hoort op
-  $or_stmt = DB::conn()->prepare("SELECT exemplaarid FROM `Orderregel` WHERE orderid=?");
-  $or_stmt->bind_param("i", $order_id);
-  $or_stmt->execute();
+  foreach($orderIdResult as $i){
+    //Haal exemplaarid van Orderregel dat bij de Order hoort op
+    $or_stmt = DB::conn()->prepare("SELECT exemplaarid FROM `Orderregel` WHERE orderid=?");
+    $or_stmt->bind_param("i", $i);
+    $or_stmt->execute();
 
-  $or_stmt->bind_result($OR_id);
-  $or_stmt->fetch();
-  $or_stmt->close();
+    $or_stmt->bind_result($OR_id);
+    $exm_id = array();
+    while($or_stmt->fetch()){
+      $exm_id[] = $OR_id;
+    }
+    $or_stmt->close();
+    print_r($exm_id);
 
-  //Haal de Filmid op van het exemplaar op
-  $exm_stmt = DB::conn()->prepare("SELECT filmid FROM `Exemplaar` WHERE id=?");
-  $exm_stmt->bind_param("i", $OR_id);
-  $exm_stmt->execute();
+    //HIER ZIT NOG EEN BUG!
+    //Haal de Filmid op van het exemplaar op
+    $exm_stmt = DB::conn()->prepare("SELECT filmid FROM `Exemplaar` WHERE id=?");
+    $exm_stmt->bind_param("i", $OR_id);
+    $exm_stmt->execute();
 
-  $exm_stmt->bind_result($exm_film_id);
-  $exm_stmt->fetch();
-  $exm_stmt->close();
+    $exm_stmt->bind_result($exm_film_id);
+    $exm_stmt->fetch();
+    $exm_stmt->close();
 
-  //Haal alles van de film op dat overeen komt met de filmid van het exemplaar
-  $exm_film_stmt = DB::conn()->prepare("SELECT id, titel, acteur, omschr, genre, img FROM `Film` WHERE id=?");
-  $exm_film_stmt->bind_param("i", $exm_film_id);
-  $exm_film_stmt->execute();
+    //Haal alles van de film op dat overeen komt met de filmid van het exemplaar
+    $exm_film_stmt = DB::conn()->prepare("SELECT id, titel, acteur, omschr, genre, img FROM `Film` WHERE id=?");
+    $exm_film_stmt->bind_param("i", $exm_film_id);
+    $exm_film_stmt->execute();
 
-  $exm_film_stmt->bind_result($film_id, $titel, $acteur, $omschr, $genre, $img);
-  $exm_film_stmt->fetch();
-  $exm_film_stmt->close();
+    $exm_film_stmt->bind_result($film_id, $titel, $acteur, $omschr, $genre, $img);
+    $exm_film_stmt->fetch();
+    $exm_film_stmt->close();
 
-  if(!empty($film_id)){
-    echo "FILM ID: " . $film_id . "<br>";
-    echo "TITEL: " . $titel . "<br>";
-  }else{
-    echo "UW WINKELMAND IS LEEG";
+    // if(!empty($film_id)){
+    //   echo "FILM ID: " . $film_id . "<br>";
+    //   echo "TITEL: " . $titel . "<br>";
+    // }else{
+    //   echo "UW WINKELMAND IS LEEG";
+    // }
   }
+
+  // //Haal exemplaarid van Orderregel dat bij de Order hoort op
+  // $or_stmt = DB::conn()->prepare("SELECT exemplaarid FROM `Orderregel` WHERE orderid=?");
+  // $or_stmt->bind_param("i", $order_id);
+  // $or_stmt->execute();
+  //
+  // $or_stmt->bind_result($OR_id);
+  // $or_stmt->fetch();
+  // $or_stmt->close();
+
+  // //Haal de Filmid op van het exemplaar op
+  // $exm_stmt = DB::conn()->prepare("SELECT filmid FROM `Exemplaar` WHERE id=?");
+  // $exm_stmt->bind_param("i", $OR_id);
+  // $exm_stmt->execute();
+  //
+  // $exm_stmt->bind_result($exm_film_id);
+  // $exm_stmt->fetch();
+  // $exm_stmt->close();
+  //
+  // //Haal alles van de film op dat overeen komt met de filmid van het exemplaar
+  // $exm_film_stmt = DB::conn()->prepare("SELECT id, titel, acteur, omschr, genre, img FROM `Film` WHERE id=?");
+  // $exm_film_stmt->bind_param("i", $exm_film_id);
+  // $exm_film_stmt->execute();
+  //
+  // $exm_film_stmt->bind_result($film_id, $titel, $acteur, $omschr, $genre, $img);
+  // $exm_film_stmt->fetch();
+  // $exm_film_stmt->close();
+
+  // if(!empty($film_id)){
+  //   echo "FILM ID: " . $film_id . "<br>";
+  //   echo "TITEL: " . $titel . "<br>";
+  // }else{
+  //   echo "UW WINKELMAND IS LEEG";
+  // }
 
 
 
