ae1aac044fd51f70cd852f0ebb3bf56448f9221d
Jeroen <grootenjeroen@hotmail.com>
2017-01-17 17:45:01 +0100

    Klant Overzicht now displays the orders that the Klant has placed (WIP)

--- resources/views/klant_overzicht.php
+++ resources/views/klant_overzicht.php
@@ -32,74 +32,156 @@ if(!empty($_SESSION['login'])){
       <h1>OVERZICHT</h1>
       <h3><b><?php echo $naam ?></b></h3>
       <hr></hr>
+      <div class="left">
+        <h4><b>INFORMATIE</b></h4>
+        <?php
+        if($edit == true && $code == $id && $action == 'edit'){
+          ?>
+          <div class="info">
+            <form method="post" action=?action=save&code=<?php echo $id ?>>
 
-      <?php
-      if($edit == true && $code == $id && $action == 'edit'){
+            <h5>ALGEMENE INFORMATIE</h5>
+            <ul class="list-group">
+              <li class="list-group-item"><b>Klantnummer: </b><?php echo $id ?></li>
+              <li class="list-group-item"><b>Naam: </b><input type="text" class="form-control" name="naam" value="<?php echo $naam ?>" required></li>
+            </ul>
+
+            <h5>CONTACT INFORMATIE</h5>
+            <ul class="list-group">
+              <li class="list-group-item"><b>Email: </b><input type="email" class="form-control" name="email" value="<?php echo $email ?>" required></li>
+              <li class="list-group-item"><b>Telefoonnummer: </b><input type="text" class="form-control" name="telefoonnummer" value="<?php echo $telefoonnummer ?>" required></li>
+            </ul>
+
+            <h5>ADRES INFORMATIE</h5>
+            <ul class="list-group">
+              <li class="list-group-item"><b>Adres: </b><input type="text" class="form-control" name="adres" value="<?php echo $adres ?>" required></li>
+              <li class="list-group-item"><b>Postcode: </b><input type="text" class="form-control" name="postcode" value="<?php echo $postcode ?>" required></li>
+              <li class="list-group-item"><b>Woonplaats: </b><input type="text" class="form-control" name="woonplaats" value="<?php echo $woonplaats ?>" required></li>
+            </ul>
+            <form method="post" action="?action=edit&code=<?php echo $id ?>">
+              <button type="submit" class="btn btn-success bestel"><li class="fa fa-floppy-o"></li> OPSLAAN</button>
+            </form>
+          </div>
+          <?php
+        }elseif($action == 'save'){
+          $stmt = DB::conn()->prepare("UPDATE `Persoon` SET `naam`=?, `email`=?, `telefoonnummer`=?, `adres`=?, `postcode`=?, `woonplaats`=? WHERE id=?");
+          $stmt->bind_param("ssssssi", $_POST['naam'], $_POST['email'], $_POST['telefoonnummer'], $_POST['adres'], $_POST['postcode'], $_POST['woonplaats'], $code);
+          $stmt->execute();
+          $stmt->close();
+          header("Refresh:0; url=/klant/overzicht");
+        }else{
         ?>
         <div class="info">
-          <form method="post" action=?action=save&code=<?php echo $id ?>>
-
-          <p><b>ALGEMENE INFORMATIE</b></p>
+          <h5>ALGEMENE INFORMATIE</h5>
           <ul class="list-group">
             <li class="list-group-item"><b>Klantnummer: </b><?php echo $id ?></li>
-            <li class="list-group-item"><b>Naam: </b><input type="text" class="form-control" name="naam" value="<?php echo $naam ?>"></li>
+            <li class="list-group-item"><b>Naam: </b><?php echo $naam?></li>
+            <li class="list-group-item"><b></b></li>
           </ul>
 
-          <p><b>CONTACT INFORMATIE</b></p>
+          <h5>CONTACT INFORMATIE</h5>
           <ul class="list-group">
-            <li class="list-group-item"><b>Email: </b><input type="email" class="form-control" name="email" value="<?php echo $email ?>"></li>
-            <li class="list-group-item"><b>Telefoonnummer: </b><input type="text" class="form-control" name="telefoonnummer" value="<?php echo $telefoonnummer ?>"></li>
+            <li class="list-group-item"><b>Email: </b><?php echo $email ?></li>
+            <li class="list-group-item"><b>Telefoonnummer: </b><?php echo $telefoonnummer ?></li>
           </ul>
 
-          <p><b>ADRES INFORMATIE</b></p>
+          <h5>ADRES INFORMATIE</h5>
           <ul class="list-group">
-            <li class="list-group-item"><b>Adres: </b><input type="text" class="form-control" name="adres" value="<?php echo $adres ?>"></li>
-            <li class="list-group-item"><b>Postcode: </b><input type="text" class="form-control" name="postcode" value="<?php echo $postcode ?>"></li>
-            <li class="list-group-item"><b>Woonplaats: </b><input type="text" class="form-control" name="woonplaats" value="<?php echo $woonplaats ?>"></li>
+            <li class="list-group-item"><b>Adres: </b><?php echo $adres ?></li>
+            <li class="list-group-item"><b>Postcode: </b><?php echo $postcode ?></li>
+            <li class="list-group-item"><b>Woonplaats: </b><?php echo $woonplaats ?></li>
           </ul>
           <form method="post" action="?action=edit&code=<?php echo $id ?>">
-            <button type="submit" class="btn btn-success bestel"><li class="fa fa-floppy-o"></li> OPSLAAN</button>
+            <input type="submit" class="btn btn-success bestel" value="PAS INFORMATIE AAN">
           </form>
         </div>
         <?php
-      }elseif($action == 'save'){
-        $stmt = DB::conn()->prepare("UPDATE `Persoon` SET `naam`=?, `email`=?, `telefoonnummer`=?, `adres`=?, `postcode`=?, `woonplaats`=? WHERE id=?");
-        $stmt->bind_param("ssssssi", $_POST['naam'], $_POST['email'], $_POST['telefoonnummer'], $_POST['adres'], $_POST['postcode'], $_POST['woonplaats'], $code);
-        $stmt->execute();
-        $stmt->close();
-        header("Refresh:0; url=/klant/overzicht");
-      }else{
-      ?>
-      <div class="info">
-        <p><b>ALGEMENE INFORMATIE</b></p>
-        <ul class="list-group">
-          <li class="list-group-item"><b>Klantnummer: </b><?php echo $id ?></li>
-          <li class="list-group-item"><b>Naam: </b><?php echo $naam?></li>
-          <li class="list-group-item"><b></b></li>
-        </ul>
-
-        <p><b>CONTACT INFORMATIE</b></p>
-        <ul class="list-group">
-          <li class="list-group-item"><b>Email: </b><?php echo $email ?></li>
-          <li class="list-group-item"><b>Telefoonnummer: </b><?php echo $telefoonnummer ?></li>
-        </ul>
-
-        <p><b>ADRES INFORMATIE</b></p>
-        <ul class="list-group">
-          <li class="list-group-item"><b>Adres: </b><?php echo $adres ?></li>
-          <li class="list-group-item"><b>Postcode: </b><?php echo $postcode ?></li>
-          <li class="list-group-item"><b>Woonplaats: </b><?php echo $woonplaats ?></li>
-        </ul>
-        <form method="post" action="?action=edit&code=<?php echo $id ?>">
-          <input type="submit" class="btn btn-success bestel" value="PAS INFORMATIE AAN">
-        </form>
-      </div>
-      <?php
-    }
+      }
 
       ?>
     </div>
+    <div class="right">
+      <h4>ORDERS</h4>
+      <?php
+      //Haal id op van Order op
+      $stmt = DB::conn()->prepare("SELECT id FROM `Order` WHERE klantid=?");
+      $stmt->bind_param("i", $id);
+      $stmt->execute();
+
+      $stmt->bind_result($order_id);
+
+      $orderIdResult = array();
+
+      while($stmt->fetch()){
+        $orderIdResult[] = $order_id;
+      }
+
+      $stmt->close();
+      if(!empty($orderIdResult)){
+        ?>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Foto</th>
+              <th>Titel</th>
+              <th>Omschrijving</th>
+            </tr>
+          </thead>
+          <tbody>
+        <?php
+        foreach($orderIdResult as $i){
+          //Haal exemplaarid van Orderregel dat bij de Order hoort op
+          $or_stmt = DB::conn()->prepare("SELECT exemplaarid FROM `Orderregel` WHERE orderid=?");
+          $or_stmt->bind_param("i", $i);
+          $or_stmt->execute();
+
+          $or_stmt->bind_result($OR_id);
+          $exm_id = array();
+          while($or_stmt->fetch()){
+            $exm_id[] = $OR_id;
+          }
+          $or_stmt->close();
+          // print_r($exm_id);
+
+          //Haal de Filmid op van het exemplaar op
+          $exm_stmt = DB::conn()->prepare("SELECT filmid FROM `Exemplaar` WHERE id=?");
+          $exm_stmt->bind_param("i", $OR_id);
+          $exm_stmt->execute();
+
+          $exm_stmt->bind_result($exm_film_id);
+          $exm_stmt->fetch();
+          $exm_stmt->close();
+
+          //Haal alles van de film op dat overeen komt met de filmid van het exemplaar
+          $exm_film_stmt = DB::conn()->prepare("SELECT id, titel, acteur, omschr, genre, img FROM `Film` WHERE id=?");
+          $exm_film_stmt->bind_param("i", $exm_film_id);
+          $exm_film_stmt->execute();
+
+          $exm_film_stmt->bind_result($film_id, $titel, $acteur, $omschr, $genre, $img);
+          $exm_film_stmt->fetch();
+          $exm_film_stmt->close();
+
+
+          if(!empty($film_id)){
+            $cover = "/cover/" . $img;
+            $URL = "/film/" . $titel;
+            $titel = strtoupper($titel);
+            $titel = str_replace('_', ' ', $titel);
+            // $bedrag = $bedrag / 100;
+            ?>
+              <tr>
+                <td><a href="<?php echo $URL ?>"><img src="<?php echo $cover ?>" class="winkelmand_img"></a></td>
+                <td><?php echo $titel ?></td>
+                <td><?php echo $omschr ?><td>
+              </tr>
+            <?php
+          }
+        }
+      }
+      ?>
+    </div>
   </div>
+</div>
 
   <?php
   }
