40ca97671bb27d7ce8ee77a662dcde466dd070ad
Jeroen <grootenjeroen@hotmail.com>
2017-01-17 13:25:30 +0100

    Now able to view all klanten in Klant Blokkeren

--- resources/views/klantblokkeren.php
+++ resources/views/klantblokkeren.php
@@ -15,7 +15,38 @@ if(!empty($_SESSION['login'])){
     <div class="panel panel-default">
       <div class="panel-body">
         <h1>KLANT BLOKKEREN</h1>
+        <?php
+        //Haal alle klanten op
+        $rol = 1;
+        $stmt = DB::conn()->prepare("SELECT id FROM `Persoon` WHERE rolid=?");
+        $stmt->bind_param('i', $rol);
+        $stmt->execute();
+        $stmt->bind_result($id);
 
+        $klanten = array();
+
+        while($stmt->fetch()){
+          $klant[] = $id;
+        }
+
+        $stmt->close();
+
+        foreach($klant as $i){
+          $stmt = DB::conn()->prepare("SELECT naam, telefoonnummer, email FROM `Persoon` WHERE id=?");
+          $stmt->bind_param('i', $i);
+          $stmt->execute();
+          $stmt->bind_result($naam, $telefoonnummer, $email);
+          $stmt->fetch();
+          $stmt->close();
+
+
+          echo "ID: " . $i . "<br>";
+          echo "Naam: " . $naam . "<br>";
+          echo "Telefoonnummer: " . $telefoonnummer . "<br>";
+          echo "Email: " . $email  . "<br><br>";
+        }
+        ?>
   <?php
+DB::conn()->close();
   }
 }
