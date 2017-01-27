f1cb1b4af03517823d9639167708b2874ccf9d5b
Jeroen <grootenjeroen@hotmail.com>
2017-01-27 11:14:01 +0100

    Added the field 'afhaaltijd' & 'ophaaltijd' to the Order column

--- resources/DB_SQL.sql
+++ resources/DB_SQL.sql
@@ -1,28 +1,79 @@
 create database tempovideo;
 use tempovideo;
-create table Rol(id int primary key, omschr varchar(45));
-create table Medewerker(id int primary key, rolid int, achternaam varchar(45), wachtwoordid int);
-create table Wachtwoord(id int primary key, wachtwoord varchar(255));
-create table Klant(id int auto_increment primary key, naam varchar(50), adres varchar(50), postcode varchar(7), woonplaats varchar(25), telefoonnummer varchar(10), email varchar(45), wachtwoordid int);
+
+create table Rol(id int primary key, 
+omschr varchar(45)
+);
+
+create table Medewerker(
+id int primary key, 
+rolid int, 
+achternaam varchar(45), 
+wachtwoordid int
+);
+
+create table Wachtwoord(
+id int primary key, 
+wachtwoord varchar(255)
+);
+
+create table Klant(
+id int auto_increment primary key, 
+naam varchar(50), 
+adres varchar(50), 
+postcode varchar(7), 
+woonplaats varchar(25), 
+telefoonnummer varchar(10), 
+email varchar(45), 
+wachtwoordid int
+);
+
 ALTER TABLE `Medewerker`
 ADD FOREIGN KEY (rolid)
 REFERENCES Rol(id);
+
 ALTER TABLE `Medewerker`
 ADD FOREIGN KEY (wachtwoordid)
 REFERENCES Wachtwoord(id);
 
-create table `Order`(id int primary key, klantid int, 
-afleverdatum VARCHAR(50), ophaaldatum VARCHAR(50), bedrag float);
+create table `Order`(
+id int primary key, 
+klantid int, 
+afleverdatum VARCHAR(50),
+aflevertijd VARCHAR(50), 
+ophaaldatum VARCHAR(50),
+ophaaltijd VARCHAR(50), 
+bedrag float
+);
 
-create table Orderregel(exemplaarid int, orderid int, primary key(exemplaarid, orderid));
+create table Orderregel(
+exemplaarid int, 
+orderid int, 
+primary key(exemplaarid, orderid)
+);
+
 ALTER TABLE `Order`
 ADD FOREIGN KEY (klantid)
 REFERENCES Klant(id);
-create table Exemplaar(id int auto_increment primary key, filmid int, statusid int, aantalVerhuur int);
+
+create table Exemplaar(
+id int auto_increment primary key, 
+filmid int, 
+statusid int, 
+aantalVerhuur int
+);
+
 ALTER TABLE `Orderregel`
 ADD FOREIGN KEY (exemplaarid)
 REFERENCES Exemplaar(id);
-create table Film(id int primary key, titel varchar(50), acteur varchar(100), omschr varchar(200));
+
+create table Film(
+id int primary key, 
+titel varchar(50), 
+acteur varchar(100), 
+omschr varchar(200)
+);
+
 ALTER TABLE `Exemplaar`
 ADD FOREIGN KEY (filmid)
 REFERENCES Film(id);
@@ -34,15 +85,22 @@ ADD img varchar(50);
 
 drop table Medewerker;
 rename table Klant to Persoon;
+
 ALTER TABLE Persoon
 ADD rolid int;
+
 ALTER TABLE `Persoon`
 ADD FOREIGN KEY (rolid)
 REFERENCES Rol(id);
+
 ALTER TABLE Film
 ADD afbeelding varchar(50);
 
-create table Status(id int primary key, omschr varchar(50));
+create table Status(
+id int primary key, 
+omschr varchar(50)
+);
+
 ALTER TABLE `Exemplaar`
 ADD FOREIGN KEY (statusid)
 REFERENCES Status(id);
@@ -58,14 +116,15 @@ INSERT INTO `Status`(id, omschr) VALUES(2, "NIET Beschikbaar");
 
 ALTER TABLE `Order`
 ADD `Afhandeling` bool;
+
 ALTER TABLE `Order`
 ADD `besteld` bool;
 
 -- SELECT * FROM `Rol`;
--- SELECT * FROM `Order`;
--- SELECT * FROM `Orderregel`;
--- SELECT * FROM `Exemplaar`;
--- SELECT * FROM `Film`;
+SELECT * FROM `Order`;
+SELECT * FROM `Orderregel`;
+SELECT * FROM `Exemplaar`;
+SELECT * FROM `Film`;
 -- SELECT * FROM `Persoon`;
 
 
