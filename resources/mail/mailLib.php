99728f4d997264e067fda3c0013cc5d4310501c3
Jeroen <grootenjeroen@hotmail.com>
2017-02-01 14:58:48 +0100

    Made the contactform work

--- resources/mail/mailLib.php
+++ resources/mail/mailLib.php
@@ -28,10 +28,10 @@ function blockMail($naam, $email){
                     Tempovideo';
   $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-  // if(!$mail->send()) {
-  //     echo 'ERROR TIJDENS HET VERSTUREN VAN DE EMAIL <br>';
-  //     echo 'Mail Error: ' . $mail->ErrorInfo;
-  // }
+  if(!$mail->send()) {
+      echo 'ERROR TIJDENS HET VERSTUREN VAN DE EMAIL <br>';
+      echo 'Mail Error: ' . $mail->ErrorInfo;
+  }
 }
 
 function confirmMail($naam, $email, $id){
@@ -92,6 +92,42 @@ function ophaalMail($order, $filmtitel, $naam, $email, $afleverdatum){
                     TempoVideo
 
                     ';
+  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+  if(!$mail->send()) {
+      echo 'ERROR TIJDENS HET VERSTUREN VAN DE EMAIL <br>';
+      echo 'Mail Error: ' . $mail->ErrorInfo;
+  }else{
+    return true;
+  }
+}
+
+function contactMail($naam, $email, $onderwerp, $bericht){
+  $mail = new PHPMailer;
+
+  // $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+  $mail->isSMTP();                                      // Set mailer to use SMTP
+  $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+  $mail->SMTPAuth = true;                               // Enable SMTP authentication
+  $mail->Username = 'mbotempovideo@gmail.com';                 // SMTP username
+  $mail->Password = 'JeroenSara7!';                           // SMTP password
+  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+  $mail->Port = 587;                                    // TCP port to connect to
+
+  $mail->setFrom('noreply@tempovideo.nl', 'Tempovideo');
+  $mail->addAddress('eigenaar@jeroengrooten.nl', 'Jeroen Grooten');     // Add a recipient
+
+  $mail->isHTML(true);                                  // Set email format to HTML
+
+  $mail->Subject = 'Nieuw contactformulier bericht';
+  $mail->Body    = '<b>NIEUW CONTACTFORMULIER BERICHT</b><br><br>
+                    Naam: '.$naam.'<br>
+                    Email: '.$email.'<br>
+                    Onderwerp: '.$email.'<br>
+                    Bericht:<br><br>
+                    '.$bericht.'
+                    <br>';
   $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
   if(!$mail->send()) {
--- resources/views/contact.php
+++ resources/views/contact.php
@@ -1,35 +1,40 @@
 <div class="panel panel-default">
+  <?php
+  if(!empty($_POST)){
+    $naam = $_POST['naam'];
+    $email = $_POST['mail'];
+    $onderwerp = $_POST['onderwerp'];
+    $bericht = $_POST['bericht'];
+    if(contactMail($naam, $email, $onderwerp, $bericht)){
+      echo "<div class='succes'><b>UW BERICHT IS VERSTUURD</b></div>";
+    }
+  }
+  ?>
   <div class="panel-body">
-
-    <div>
-        <div>
-        <h2> Contactformulier:</h2>
-            <div class="inner contact">
-                <div class="contact-form">
-                    <form id="contact-us" method="post" action="#">
-                        <div class="col-xs-6 wow animated slideInLeft" data-wow-delay=".5s">
-                            <input type="text" name="name" id="name" required="required" class="form" placeholder="Naam" />
-                            <input type="email" name="mail" id="mail" required="required" class="form" placeholder="Email" />
-                            <input type="text" name="subject" id="subject" required="required" class="form" placeholder="Onderwerp" />
-                        </div>
-                        <div class="col-xs-6 wow animated slideInRight" data-wow-delay=".5s">
-                            <textarea name="message" id="message" class="form textarea"  placeholder="Uw bericht"></textarea>
-                        </div>
-                        <div class="relative fullwidth col-xs-12">
-                            <button type="submit" id="submit" name="submit" class="form-btn semibold">Verzenden</button>
-                        </div>
-                        <div class="clear"></div>
-                    </form>
-                </div>
+    <h2> Contactformulier:</h2>
+        <div class="inner contact">
+            <div class="contact-form">
+                <form id="contact-us" method="post">
+                    <div class="col-xs-6 wow animated slideInLeft" data-wow-delay=".5s">
+                        <input type="text" name="naam" id="name" required="required" class="form" placeholder="Naam" />
+                        <input type="email" name="mail" id="mail" required="required" class="form" placeholder="Email" />
+                        <input type="text" name="onderwerp" id="subject" required="required" class="form" placeholder="Onderwerp" />
+                    </div>
+                    <div class="col-xs-6 wow animated slideInRight" data-wow-delay=".5s">
+                        <textarea name="bericht" id="message" class="form textarea"  placeholder="Uw bericht"></textarea>
+                    </div>
+                    <div class="relative fullwidth col-xs-12">
+                        <button type="submit" id="submit" name="submit" class="form-btn semibold">Verzenden</button>
+                    </div>
+                    <div class="clear"></div>
+                </form>
             </div>
         </div>
       <div>
-          <h2>Contactgegevens: </h2>
-          TempoVideo <br>
-          info@tempovideo.nl <br>
-          088-1234567
+        <h2>Contactgegevens: </h2>
+        TempoVideo <br>
+        info@tempovideo.nl <br>
+        088-1234567
       </div>
-    </div>
   </div>
 </div>
-
