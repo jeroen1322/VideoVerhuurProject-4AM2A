02fd45b90f2a7869eb5ac252c69cd8d597b9a334
Jeroen <grootenjeroen@hotmail.com>
2017-02-03 13:53:34 +0100

    Fixed responsiveness of the homepage carousel

--- public/js/js.js
+++ public/js/js.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
-
-  $('.nieuw_film_slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    speed: 600
-  });
+  console.log($(window).width());
+  if($(window).width() < 610){
+    $('.nieuw_film_slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      speed: 600,
+      responsive: true
+    });
+  }else if($(window).width() < 710 && $(window).width() > 610){
+    $('.nieuw_film_slider').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      speed: 600,
+      responsive: true
+    });
+  }else{
+    $('.nieuw_film_slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      dots: true,
+      speed: 600,
+      responsive: true
+    });
+  }
   $('.afleverDatum').hide();
   $('.nee').click(function(){
     $('.afleverDatum').show("fast");
